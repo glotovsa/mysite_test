3f218e78d134090ace4161204b92b48a1fd93592
glotovsa <k221@ya.ru>
2019-08-01 08:38:45 +0000

    Первая загрузка проекта...

---
+++ start_web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install apt-transport-https
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+sudo docker ps -a
+
+sudo tar -xvf archive.tar.gz
+
+sudo docker-compose build
+sudo docker-compose up
+
